ea39ba646af8fe12e26c71655ff1aba837060a3c
sunxiaochuan481-arch <sunxiaochuan481@gmail.com>
2026-06-01 23:42:46 +0800

    github pages push2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/v2-my-card/' : '/',
   plugins: [react()],
 })
